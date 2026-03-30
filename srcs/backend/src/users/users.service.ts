54df1ded86566324882d7296048b3d38b0076637
MatthieuDh <174446229+MatthieuDh@users.noreply.github.com>
2026-03-30 13:53:19 +0200

    added password hashing

--- srcs/backend/src/users/users.service.ts
+++ srcs/backend/src/users/users.service.ts
@@ -2,12 +2,26 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService} from '../prisma/prisma.service'
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  async create(createUserDto: CreateUserDto) {
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+    return this.prisma.user.create({
+      data: {
+        ...createUserDto,
+        password: hashedPassword,
+      },
+      select:{
+        id: true,
+        username: true,
+        role: true,
+        avatar: true,
+        createdAt: true,
+      }
+    });
   }
 
   async findAll() {
@@ -35,11 +49,48 @@ export class UsersService {
     });
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    return this.prisma.update({
+      where: { id },
+      data: UpdateUserDto,
+      select: {
+        id: true,
+        username: true,
+        role: true,
+        avatar: true,
+        createdAt: true,
+        updatedAt: true,
+      }
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+    async updateByName(username: string, updateUserDto: UpdateUserDto) {
+    return this.prisma.update({
+      where: { username },
+      data: UpdateUserDto,
+      select: {
+        id: true,
+        username: true,
+        role: true,
+        avatar: true,
+        createdAt: true,
+        updatedAt: true,
+      }
+    });
+  }
+
+  async remove(id: number) {
+    return this.prisma.user.delete({
+      where: { id }
+    });
+  }
+
+    async removeByName(username: string) {
+    return this.prisma.user.delete({
+      where: { username }
+    });
   }
 }
+
+
+
