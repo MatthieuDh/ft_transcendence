1202cf46da38b314ff67753eeb073bc779c9be01
MatthieuDh <174446229+MatthieuDh@users.noreply.github.com>
2026-03-31 14:31:05 +0200

    setting up auth resource

--- srcs/backend/src/auth/auth.module.ts
+++ srcs/backend/src/auth/auth.module.ts
@@ -2,10 +2,19 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule, 
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: {expiresIn: '15m'},
+  }),
+],
   controllers: [AuthController],
   providers: [AuthService],
+  exports: [AuthService],
 })
 export class AuthModule {}
--- srcs/backend/src/auth/auth.service.ts
+++ srcs/backend/src/auth/auth.service.ts
@@ -1,15 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service'
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { access } from 'fs';
+
+interface JwtPayload {
+  sub: number;
+  username: string;
+}
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService){}
+  constructor(
+    private usersService: UsersService, 
+    private jwtService: JwtService
+  ){}
 
-  async signIn(username: string, pass:string): Promise<any> {
+  async signIn(username: string, pass:string): Promise<{access_token: string, refresh_token: string}> {
     const user = await this.usersService.findByName(username);
+    if (!user) {
+      throw new UnauthorizedException();
+    }
     const isPasswordValid = await bcrypt.compare(pass, user.password);
-    if (!isPasswordValid) return null;
-    return user;
+    if (!isPasswordValid) {
+      throw new UnauthorizedException();
+    }
+    const payload = { sub: user.id, username: user.username };
+    return {
+      access_token: await this.jwtService.signAsync(payload, {expiresIn: '15m'}),
+      refresh_token: await this.jwtService.signAsync(payload, {expiresIn: '7d'}),
+    };
+  }
+
+  async refresh(refreshToken: string): Promise<{access_token: string}> {
+    try {
+      const payload = this.jwtService.verifyAsync<JwtPayload>(refreshToken, {secret: process.env.JWT_SECRET});
+      return {
+              access_token: await this.jwtService.signAsync(payload, {expiresIn: '15m'}),
+      }
+    } catch {
+      throw new UnauthorizedException();
+    }
   }
 }
