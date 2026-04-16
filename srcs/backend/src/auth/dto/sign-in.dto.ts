589f123c0f50d89be2a219e8219181ea1c14532b
MatthieuDh <174446229+MatthieuDh@users.noreply.github.com>
2026-04-16 16:50:24 +0200

    fixed typescript version issue

--- srcs/backend/src/auth/dto/sign-in.dto.ts
+++ srcs/backend/src/auth/dto/sign-in.dto.ts
@@ -2,7 +2,7 @@ import { IsString, MinLength } from 'class-validator';
 
 export class SignInDto {
     @IsString()
-    username!: string;
+    username: string;
 
     @IsString()
     @MinLength(8)
