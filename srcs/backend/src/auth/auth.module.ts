f731cc7c74947b511c0aa307852f331d734bb3cf
siebe de boeck <siebedeboeck@MacBook-Air-van-siebe.local>
2026-04-21 13:48:08 +0200

    added jwtstrategy import

--- srcs/backend/src/auth/auth.module.ts
+++ srcs/backend/src/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { UsersModule } from 'src/users/users.module';
 import { JwtModule, JwtService } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { GoogleStrategy } from './strategies/google.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports: [
@@ -19,7 +20,7 @@ import { GoogleStrategy } from './strategies/google.strategy';
   PassportModule
 ],
   controllers: [AuthController],
-  providers: [AuthService, GoogleStrategy],
+  providers: [AuthService, GoogleStrategy, JwtStrategy],
   exports: [AuthService],
 })
 export class AuthModule {}
