12ed2dbebcb549b0e00034b09abb6bd33d16e33d
siebe de boeck <siebedeboeck@MacBook-Air-van-siebe.local>
2026-04-21 13:15:00 +0200

    added migrations

--- srcs/backend/prisma/migrations/20260417114905_add_deadlines/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Project" ADD COLUMN     "deadline" TIMESTAMP(3);
-
--- AlterTable
-ALTER TABLE "Task" ADD COLUMN     "deadline" TIMESTAMP(3);
--- srcs/backend/prisma/migrations/20260417122843_init/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Project" ADD COLUMN     "deadlineNotified" BOOLEAN NOT NULL DEFAULT false;
-
--- AlterTable
-ALTER TABLE "Task" ADD COLUMN     "deadlineNotified" BOOLEAN NOT NULL DEFAULT false;
