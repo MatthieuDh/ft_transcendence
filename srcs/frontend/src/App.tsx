092d89b41347cec830fb36bb99b5348ab8082594
MatthieuDh <174446229+MatthieuDh@users.noreply.github.com>
2026-04-30 16:06:26 +0200

    added ProfilePage to routing

--- srcs/frontend/src/App.tsx
+++ srcs/frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import { ProfilePage } from './pages/ProfilePage';
 
 export default function App() {
   return (
@@ -11,6 +12,9 @@ export default function App() {
 
         {/* RergisterPafe redirect */}
         <Route path="/register" element={<RegisterPage />} />
+
+        {/* ProfilePage redirect */}
+        <Route path="/profile/:userId" element={<ProfilePage />} />
 
         {/* for now if you go to an page that does not exist i route to login */} 
         <Route path="*" element={<Navigate to="/login" />} />
